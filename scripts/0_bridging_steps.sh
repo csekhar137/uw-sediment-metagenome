39a5a5c5cc1b22c34d27bf93c975fe9c4e8ed086
Aditya Bandla <abandla@nus.edu.sg>
2019-08-22 23:10:27 +0800

    bridging steps

--- scripts/0_bridging_steps.sh
+++ scripts/0_bridging_steps.sh
@@ -1,4 +1,5 @@
 # Concatenate reads
+# Single sample assemblies
 # Set input and output directories
 PROJECT=INSERT PROJECT NAME HERE
 INP=${SCRATCH}/datasets/${PROJECT}/data/processed/2_quality_trimmed
@@ -8,3 +9,4 @@ for NC in $(ls | cut -c-6 | uniq) ; do
 cat $(ls | grep ${NC} | grep '_R1_') > ${OUT}/${NC}_R1_QT.fastq.gz
 cat $(ls | grep ${NC} | grep '_R2_') > ${OUT}/${NC}_R2_QT.fastq.gz
 done
+
